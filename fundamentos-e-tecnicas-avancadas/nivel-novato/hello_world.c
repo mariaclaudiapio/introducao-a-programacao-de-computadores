b65243040cd07abf4107e57be40d0ac6a1c841fe
mariaclaudiapio <mariaclaudiapio@gmail.com>
2025-04-27 15:11:20 -0300

    Commit da Aula 1 Hello World

---
+++ fundamentos-e-tecnicas-avancadas/nivel-novato/hello_world.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    // Cabeçalho
+    puts("");
+    puts("******************************");
+    puts("**   Aula 1 - Hello World   **");
+    puts("******************************");
+    puts("");
+
+    // Impressão da mensagem na tela do terminal
+    printf("Hello, World! 😃\n");
+
+    puts("");
+
+    return 0;
+}
