06d7098f1d9acfaf6dc4e7c2f884c2af2c2c3fe9
mariaclaudiapio <mariaclaudiapio@gmail.com>
2025-04-27 15:51:46 -0300

    Aula 2 - Variáveis e Tipos de Dados

---
+++ fundamentos-e-tecnicas-avancadas/nivel-novato/variaveis_e_tipos_de_dados.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    // Cabeçalho
+    puts("");
+    puts("*********************************************");
+    puts("**   Aula 2 - Variáveis e Tipos de Dados   **");
+    puts("*********************************************");
+    puts("");
+
+    // Declaração
+    int idade = 42;    
+    float altura = 1.55;
+    char sexo = 'F';
+    char nome[30] = "Maria Cláudia";
+
+    printf("A idade é de %d anos.\n", idade);
+
+    puts(" ");
+
+    return 0;
+}
