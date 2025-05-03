3b30c325ffb54b59946e784b11b1c25d836bc5eb
mariaclaudiapio <mariaclaudiapio@gmail.com>
2025-05-02 22:40:40 -0300

    criando Nível Mestre

---
+++ super-trunfo/fundamentos-e-tecnicas-avancadas/nivel-mestre/modificadores_de_tipos.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    // Cabeçalho
+    puts("");
+    puts("******************************************");
+    puts("**   Aula 03 - Modificadores de tipos   **");
+    puts("******************************************");
+    puts("");
+
+    
+
+    puts("");
+
+    return 0;
+}
