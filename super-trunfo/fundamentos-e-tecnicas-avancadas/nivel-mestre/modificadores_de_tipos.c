5d1f172c894f4e907efa518e4789c93df4e75e05
mariaclaudiapio <mariaclaudiapio@gmail.com>
2025-05-03 15:37:00 -0300

    incluindo informações sobre modificadores de tipos: short long e unsigned int

--- super-trunfo/fundamentos-e-tecnicas-avancadas/nivel-mestre/modificadores_de_tipos.c
+++ super-trunfo/fundamentos-e-tecnicas-avancadas/nivel-mestre/modificadores_de_tipos.c
@@ -9,7 +9,40 @@ int main()
     puts("******************************************");
     puts("");
 
+    // long: aumenta capacidade das variáveis primitivas
+    // No double, o long aumenta a precisão (atenção, pois os valores tratados são alterados de acordo com o ambiente)
+    // sort: economiza memória (útil para sistemas embarcados, por exemplo)
     
+    // unsigned: variáveis que armazenam apenas valores positivos (dobra a faixa positiva da variável, em comparação com o signed)
+    int numeroComSinal = 3000000000;
+    unsigned int numeroSemSinal = 3000000000;
+    
+    printf("Valor do número com sinal: %d.\n", numeroComSinal); // o valor fica positivo porque "estoura" a capacidade de armazenamento da variável
+    printf("Valor do número sem sinal: %u.\n", numeroSemSinal);
+   
+    puts("Tamanho das variáveis por tipo: ");
+    printf("Inteiro: %d bytes.\n", sizeof(int));
+    printf("Unsigned: %d bytes.\n", sizeof(unsigned int));
+    printf("Long int: %d bytes.\n", sizeof(long int));
+    printf("Long long int: %d bytes.\n", sizeof(long long int));
+    printf("Float: %d bytes.\n", sizeof(float));
+    printf("Double float: %d.\n", sizeof(double));
+    printf("Short %d\n", sizeof(short));
+
+    //exemplo da aula
+    unsigned long int numeroGrandePositivo = 4000000000;
+    unsigned int numeroPositivo = 4000000000;
+    long int numeroGrande = 4000000000;
+    int numero = 4000000000;
+    short int numeroPequeno = 32768;
+    
+
+    puts("Exemplo da aula");
+    printf("Número positivo grande: %lu\n", numeroGrandePositivo);
+    printf("Número positivo: %u\n", numeroPositivo);
+    printf("Número grande: %ld\n", numeroGrande);
+    printf("Número: %d\n", numero);
+    printf("Número pequeno: %d", numeroPequeno);
 
     puts("");
 
---
+++ super-trunfo/fundamentos-e-tecnicas-avancadas/nivel-mestre/readme.md
@@ -0,0 +1,9 @@
+<h1 style="text-align: center;">Modificadores de tipos de dados</h1>
+<h2 style="text-align: center;">Práticas Recomendadas</h2>
+<ol>
+    <li>Escolha o tipo de dado apropriado;</li>
+    <li>Verifique a compatibilidade de tipos;</li>
+    <li>Use especificadores de formatos corretos;</li>
+    <li>Verifique o tamanho do tipo de dados para o ambiente;</li>
+    <li>Considere o seu ambiente de desenvolvimento.</li>
+</ol>
