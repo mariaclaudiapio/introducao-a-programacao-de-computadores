380ed41ea6b0201840a25cce50bc4541a4318da7
mariaclaudiapio <mariaclaudiapio@gmail.com>
2025-05-19 21:51:01 -0300

    Introdução aos Loops Aninhados

---
+++ estruturas-de-repeticao/nivel-aventureiro/loops_aninhados.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\n****************************************\n");
+    printf("**   Introdução aos Loops Aninhados   **\n");
+    printf("****************************************\n");
+
+    return 0;
+}
