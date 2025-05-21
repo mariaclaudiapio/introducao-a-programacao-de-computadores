9925bb76ca7c574829ede64d5c205b62ec4eec72
mariaclaudiapio <mariaclaudiapio@gmail.com>
2025-05-20 21:02:49 -0300

    Loops aninhados

---
+++ estruturas-de-repeticao/nivel-aventureiro/hora_de_codar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    
+    for(i = 1; i <= 5; i++)
+    {
+        char letra = 'A';
+        for(j = 1; j <= i; j++)
+        {
+            printf(" %c", letra);
+            letra++;
+        }
+        puts("");
+    }
+}
--- estruturas-de-repeticao/nivel-aventureiro/loops_aninhados.c
+++ estruturas-de-repeticao/nivel-aventureiro/loops_aninhados.c
@@ -2,9 +2,19 @@
 
 int main()
 {
-    printf("\n****************************************\n");
-    printf("**   Introdução aos Loops Aninhados   **\n");
-    printf("****************************************\n");
+    printf("*************************\n");
+    printf("**   Loops Aninhados   **\n");
+    printf("*************************\n");
+
+    for(int i = 1; i <= 10; i++)
+    {
+        printf("\nTabuada do %d:", i);
+        for(int j = 1; j <= 10; j++)
+        {
+            printf("\n%d x %d = %d ", i, j, (i * j));
+        }
+        printf("\n");
+    }
 
     return 0;
 }
