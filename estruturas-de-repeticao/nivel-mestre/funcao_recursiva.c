09afe6a0ef2c79dab7c553647a9f80c30d271b65
mariaclaudiapio <mariaclaudiapio@gmail.com>
2025-05-22 20:57:08 -0300

    Recursividade

---
+++ estruturas-de-repeticao/nivel-mestre/funcao_recursiva.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// a função recursiva evoca a si mesma
+
+// funçao recursiva que decrementa valores
+void funcaoRecursiva(int numero)
+{
+    if(numero > 0)
+    {
+        printf("%d \n", numero);
+
+        funcaoRecursiva(numero - 1);
+    }
+}
+
+int main()
+{
+    int quantidade = 10;
+
+    printf("Contagem regressiva...\n");
+
+    funcaoRecursiva(quantidade);
+
+    return 0;
+}
